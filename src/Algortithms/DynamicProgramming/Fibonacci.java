276ed52bc1f6565c11ec2ea4165b7c050fc459d5
Nameer-kp <kpnameer@yahoo.com>
2020-10-02 23:28:59 +0530

    DFS Recursive

---
+++ src/Algortithms/DynamicProgramming/Fibonacci.java
@@ -0,0 +1,4 @@
+package Algortithms.DynamicProgramming;
+
+public class Fibonacci {
+}
--- src/Algortithms/Searching/DepthFirstSearch.java
+++ src/Algortithms/Searching/DepthFirstSearch.java
@@ -8,14 +8,17 @@ import java.util.List;
 public class DepthFirstSearch {
     public static void DepthFirstSearchPostOrder(BinarySearchTree tree)
     {
+        List<Integer> list = new ArrayList<>();
+        System.out.println("PostOrder DFS :"+traversePostOrder(tree.getRoot(),list));
     }
 public static void DepthFirstSearchInOrder(BinarySearchTree tree)
     {
         List<Integer> list = new ArrayList<>();
-        System.out.println(traverseInOrder(tree.getRoot(),list));
+        System.out.println("InOrder DFS :"+traverseInOrder(tree.getRoot(),list));
 }
 public static void DepthFirstSearchPreOrder(BinarySearchTree tree){
-
+    List<Integer> list = new ArrayList<>();
+    System.out.println("PreOrder DFS :"+traversePreOrder(tree.getRoot(),list));
     }
     public static List<Integer> traverseInOrder(BinaryNode node,List<Integer> list){
         if(node.getLeft()!=null){
@@ -24,6 +27,29 @@ public static void DepthFirstSearchPreOrder(BinarySearchTree tree){
         list.add(node.getValue()); // this adds the element to the list
         if(node.getRight()!=null){
             traverseInOrder(node.getRight(),list); //this goes to the right nodes
+        }
+
+        return list;
+    }
+    public static List<Integer> traversePostOrder(BinaryNode node,List<Integer> list){
+        if(node.getLeft()!=null){
+            traversePostOrder(node.getLeft(),list); //this recursion goes to very left nodes
+        }
+
+        if(node.getRight()!=null){
+            traversePostOrder(node.getRight(),list); //this goes to the right nodes
+        }
+        list.add(node.getValue()); // this adds the element to the list
+
+        return list;
+    }
+    public static List<Integer> traversePreOrder(BinaryNode node,List<Integer> list){
+        list.add(node.getValue()); // this adds the element to the list
+        if(node.getLeft()!=null){
+            traversePreOrder(node.getLeft(),list); //this recursion goes to very left nodes
+        }
+        if(node.getRight()!=null){
+            traversePreOrder(node.getRight(),list); //this goes to the right nodes
         }
 
         return list;
@@ -40,5 +66,7 @@ public static void DepthFirstSearchPreOrder(BinarySearchTree tree){
         tree.insert(170);
         tree.insert(15);
         DepthFirstSearchInOrder(tree);
+        DepthFirstSearchPreOrder(tree);
+        DepthFirstSearchPostOrder(tree);
     }
 }
